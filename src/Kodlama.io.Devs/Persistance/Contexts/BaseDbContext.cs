c78f382dd4b6ce7622b18f210f3d10f4d0f1889c
TUBA <TUBA@CON-PC>
2022-09-15 13:18:23 +0300

    Add project files.

---
+++ src/Kodlama.io.Devs/Persistance/Contexts/BaseDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+        public BaseDbContext(IConfiguration configuration, DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(a =>
+            {
+                a.ToTable("ProgrammingLanguage").HasKey(k => k.Id);
+                a.Property(p => p.Id).HasColumnName("Id");
+                a.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            ProgrammingLanguage[] programmingLanguageEntitySeeds = { new(1,"C#"), new(2,"Java") };
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageEntitySeeds);
+
+        }
+    }
+}
